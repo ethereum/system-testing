fa7fb438e6ede4fc341f4b933b25b7a3c4a2391b
Heiko Heiko <heiko@heiko.org>
2014-12-11 14:27:27 +0100

    use substitutions

--- logging/teees.py
+++ logging/teees.py
@@ -5,15 +5,13 @@ teees | tee like tool to log to elasticsearch
 # -u     : unbuffered binary stdout and stderr (also PYTHONUNBUFFERED=x)
 
 operation:
-    read lines from pipes stdin, stderr
+    read lines from pipes stdin (redirect stderr)
         if not valid json format:
             make fallback json format
         add timestamp
         add key/values parsed from sys.args
         send to an elasticsearch instance
-        write to stdout/stderr
-
-
+        echo original to stdout (necessary?)
 
 Kibana / Logstash:
 indices should be formatted like logstash-YYYY.MM.DD.
@@ -21,11 +19,6 @@ You can change the pattern Kibana is looking for, but we won't do that here
 logs must have a timestamp, stored in the @timestamp field.
 It's also nice to put the message part in the message field
 cause Kibana shows it by default
-
-
-timestamp
-    2009-11-15T14:12:12
-
 
 ToDo:
     cmd line args:
@@ -44,8 +37,10 @@ ToDo:
 """
 import sys
 import json
+import datetime
 import elasticsearch
 from logstash_formatter import LogstashFormatter
+from event_names_map import substitutions
 
 if len(sys.argv) < 2:
     print "usage: loggging_app | teees.py <elasticsearch_host:port> <extra_key,value_pairs> ..."
@@ -56,7 +51,7 @@ extra = dict(x.split(',') for x in sys.argv[2:])
 
 ### ES
 es = elasticsearch.Elasticsearch(es_endpoint)
-es_index_name = 'logstash-2014.12.10' # logstash-YYYY.MM.DD
+es_index_name = 'logstash-%s' % datetime.datetime.utcnow().strftime('%Y.%m.%d') # logstash-YYYY.MM.DD
 es_doc_type = 'ethlog'
 
 def es_log(doc):
@@ -69,8 +64,18 @@ while True:
     try:
         d = json.loads(l)
     except ValueError:
-        d = dict(msg='raw', raw=l)
+        d = dict(event='notjson', log_line=l, logging_error='raw input')
+
+    # check that there is an event
+    if not 'event' in d:
+        d['event'] = 'notset'
+        d['logging_error'] = 'event_not_set'
+
+    # substitute event name
+    d['event'] = substitutions.get(d['event'], d['event'])
+
     # send to elasticsearch
     es_log(lsformatter.format(d))
+
     # tee like echo
-    print l
+#    print l
