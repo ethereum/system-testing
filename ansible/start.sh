3ea9b28f7f019cde50d8b0bf74b17b08770fa333
sveneh <sven@ethdev.com>
2015-02-10 14:02:40 +0100

    made testbest-setup more failproof with other regions, removed start.sh as it is obsoleted by testbed-setup.sh

--- ansible/start.sh
+++
@@ -1,7 +0,0 @@
-if ! ansible-playbook -i inventory/hosts ec2-setup.yml; then echo 'aborting'; exit 1; fi
-sleep 120;
-echo 'sleeping a minute to give nodes time to boot';
-sleep 60;
-if ! ansible-playbook client-setup.yml; then echo 'aborting'; exit 1; fi
-if ! ansible-playbook client-setup-bootstrap.yml; then echo 'aborting'; exit 1; fi
-if ! ansible-playbook elarch-setup.yml; then echo 'aborting'; exit 1; fi
--- testbed-setup.sh
+++ testbed-setup.sh
@@ -2,16 +2,6 @@
 
 EC2_REGION=${1:-us-west-1} 
 SCRIPTDIR=`dirname $(readlink --canonicalize $0)`
-cd $SCRIPTDIR/ansible
-
-if ! grep --silent '\[local\]' "/etc/ansible/hosts"; then
-   	echo "Needs an entry like"
-   	echo "[local]"
-	echo "localhost ansible_connection=local"
-	echo "in /etc/ansible/hosts"
-	echo "to function properly. Please fix"
-	exit -1
-fi
 
 if ! grep --silent '\[Credentials\]' ~/.boto; then
    	echo "No AWS credentials set. Please fix"
@@ -19,13 +9,18 @@ if ! grep --silent '\[Credentials\]' ~/.boto; then
 fi
 
 if [ $EC2_REGION != "us-west-1" ] ; then
-	echo "Make sure you have adjusted ec2.ini for your region! Otherwise will fail"
+	echo "This is not the default region, and not testet thoroughly."
 	read -p "[Enter] to continue."
 fi
 
+cd $SCRIPTDIR/ansible
+
+# the region is also stored in the inventory control file, adjust there, too
+sed --in-place=.bak "s/^regions =.*/regions = $EC2_REGION/" inventory/ec2.ini
+
 echo "Setting up in $EC2_REGION region."
 
-ansible-playbook ec2-setup.yml	 			--extra-vars=ec2_region=$EC2_REGION && \
+ansible-playbook ec2-setup.yml	 			--extra-vars=ec2_region=$EC2_REGION --inventory-file=inventory/hosts && \
 ansible-playbook client-setup.yml 			--extra-vars=ec2_region=$EC2_REGION && \
 ansible-playbook client-setup-bootstrap.yml --extra-vars=ec2_region=$EC2_REGION && \
 ansible-playbook elarch-setup.yml 			--extra-vars=ec2_region=$EC2_REGION 
