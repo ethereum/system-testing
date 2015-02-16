43a80fbbcfeaa0a901ef536be8f9d573d9b747b5
sveneh <sven@ethdev.com>
2015-02-16 12:29:17 +0100

    events for the basic connection scenario

--- logging/events.py
+++ logging/events.py
@@ -46,13 +46,15 @@ Testing mode which reads logs of a client and checks
 
 """
 
-address = 'hex40'
-guid = 'hex128'
-endpoint = 'host:port'
-hexhash = 'hex64'
+address = 'hex40, e.g. 0x0123456789abcdef0123456789abcdef01234567'
+protocol_version = 'int, e.g. 52'
+guid = 'hex128, e.g. 0x0123456789abcdef... 128 digits'
+client_addr = 'host:port, e.g. 10.46.56.35:30303 or poc-8.ethdev.com:30303'
+client_impl = 'Impl/OS/version, e.g. Go/Linux/0.8.2'
+hexhash = 'hex64, e.g. 0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
 hexrlp = 'hexN'
 timestamp = 'YYYY-MM-DDTHH:MM:SS.SSSSSSZ'
-level = 'debug'
+num_connections = 'int, e.g. 4 - number of other nodes this client is currently connected to'
 
 class Event(object):
     """
@@ -62,7 +64,7 @@ class Event(object):
         inherit to set repeating key/value information requirements
     """
     events = []
-    defaults = dict(guid=guid, ts=timestamp, level=level)
+    defaults = dict(ts=timestamp)
 
     def __init__(self, name, **kargs):
         self.name = name
@@ -79,28 +81,28 @@ class Event(object):
 ##########################################################
 
 # Startup
-Event('starting', eth_version=0, version_string='', coinbase=address)
+Event('starting', comment='one of the first log events, before any operation is started', level='info', eth_version=protocol_version, client_impl=client_impl)
 
 # P2P
 class P2PEvent(Event):
-    defaults = dict(remote_id=guid, num_connections=0)
+    defaults = dict(remote_id=guid, num_connections=num_connections)
     defaults.update(Event.defaults)
 
-P2PEvent('p2p.connecting', remote_endpoint=endpoint)
-P2PEvent('p2p.connected')
-P2PEvent('p2p.handshaked', remote_capabilities=[])
-P2PEvent('p2p.disconnected')
-P2PEvent('p2p.disconnecting', reason='')
+# P2PEvent('p2p.connecting', remote_addr=client_addr)
+P2PEvent('p2p.connected', comment='as soon as a successful connetion to another node is established', level='info', remote_addr=client_addr, remote_impl=client_impl)
+#P2PEvent('p2p.handshaked', remote_capabilities=[])
+#P2PEvent('p2p.disconnected')
+#P2PEvent('p2p.disconnecting', reason='')
 # more precise reasons
-P2PEvent('p2p.disconnecting.bad_handshake', reason='')
-P2PEvent('p2p.disconnecting.bad_protocol', reason='')
+# P2PEvent('p2p.disconnecting.bad_handshake', reason='')
+# P2PEvent('p2p.disconnecting.bad_protocol', reason='')
 # e.g. if a peer doesn't deliver (txs, blks, ...) as expected
-P2PEvent('p2p.disconnecting.reputation', reason='')
+# P2PEvent('p2p.disconnecting.reputation', reason='')
 # e.g. if there where better connection options found
-P2PEvent('p2p.disconnecting.dht', reason='')
+# P2PEvent('p2p.disconnecting.dht', reason='')
 # if a peer sends rough info
-P2PEvent('p2p.eth.disconnecting.bad_block', reason='')
-P2PEvent('p2p.eth.disconnecting.bad_tx', reason='')
+# P2PEvent('p2p.eth.disconnecting.bad_block', reason='')
+#P2PEvent('p2p.eth.disconnecting.bad_tx', reason='')
 
 
 # Blocks
@@ -109,28 +111,28 @@ class BlockEvent(Event):
     defaults.update(Event.defaults)
 
 # created blocks
-BlockEvent('eth.newblock.mined', block_hexrlp=hexrlp)
-BlockEvent('eth.newblock.broadcasted')
+# BlockEvent('eth.newblock.mined', block_hexrlp=hexrlp)
+# BlockEvent('eth.newblock.broadcasted')
 
 class ReceivedBlockEvent(BlockEvent):
     defaults = dict(remote_id=guid)
     defaults.update(BlockEvent.defaults)
 
 # received blocks
-ReceivedBlockEvent('eth.newblock.received')
-ReceivedBlockEvent('eth.newblock.is_known')
-ReceivedBlockEvent('eth.newblock.is_new')
-ReceivedBlockEvent('eth.newblock.missing_parent')
-ReceivedBlockEvent('eth.newblock.is_invalid', reason='')
-# previously unknown block w/ block.number < head.number
-ReceivedBlockEvent('eth.newblock.chain.is_older')
-# block which appends to the chain w/ highest difficulty (after appending)
-ReceivedBlockEvent('eth.newblock.chain.is_canonical')
-# block which appends to a chain which has not the highest difficulty
-ReceivedBlockEvent('eth.newblock.chain.not_canonical')
-# if the block makes adds to a differnt chain which then has the highest total difficult.
-# i.e. block.prev != head.prev != head
-ReceivedBlockEvent('eth.newblock.chain.switched', old_head_hash=hexhash)
+#ReceivedBlockEvent('eth.newblock.received')
+#ReceivedBlockEvent('eth.newblock.is_known')
+#ReceivedBlockEvent('eth.newblock.is_new')
+#ReceivedBlockEvent('eth.newblock.missing_parent')
+#ReceivedBlockEvent('eth.newblock.is_invalid', reason='')
+## previously unknown block w/ block.number < head.number
+#ReceivedBlockEvent('eth.newblock.chain.is_older')
+## block which appends to the chain w/ highest difficulty (after appending)
+#ReceivedBlockEvent('eth.newblock.chain.is_canonical')
+## block which appends to a chain which has not the highest difficulty
+#ReceivedBlockEvent('eth.newblock.chain.not_canonical')
+## if the block makes adds to a differnt chain which then has the highest total difficult.
+## i.e. block.prev != head.prev != head
+#ReceivedBlockEvent('eth.newblock.chain.switched', old_head_hash=hexhash)
 
 
 # Transactions
@@ -141,11 +143,11 @@ class TXEvent(Event):
 
 # scope of tx events is only for those received over the wire
 # not those included in blocks (discuss!)
-TXEvent('eth.tx.created', tx_hexrlp=hexrlp)
-TXEvent('eth.tx.received', remote_id=guid)
-TXEvent('eth.tx.broadcasted')
-TXEvent('eth.tx.validated')
-TXEvent('eth.tx.is_invalid', reason='')
+#TXEvent('eth.tx.created', tx_hexrlp=hexrlp)
+#TXEvent('eth.tx.received', remote_id=guid)
+#TXEvent('eth.tx.broadcasted')
+#TXEvent('eth.tx.validated')
+#TXEvent('eth.tx.is_invalid', reason='')
 
 
 # used by teees.py to wrap and annotate bad none json logs
