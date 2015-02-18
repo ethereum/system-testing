32a0006acc7d7ca873025ba52eeff841a7b9f72d
sveneh <sven@ethdev.com>
2015-02-18 17:17:05 +0100

    adjusted to support Go networking poc-8, using the automatic Docker build from gitub go-ethereum, refactored global variables into separate file. Closes #30 , again.

--- scenarios/clients.py
+++ scenarios/clients.py
@@ -3,9 +3,8 @@ random notes:
 ansible --overwrite
 http://docs.ansible.com/developing_inventory.html
 http://docs.ansible.com/playbooks_variables.html#passing-variables-on-the-command-line
-
-docker_run_args = []
 """
+
 from base import Inventory
 import nodeid_tool
 import json
@@ -16,15 +15,21 @@ import os
 
 key_file = '../ansible/system-testing.pem'
 
-# needs some for go, too
-docker_run_args = '--logging :debug --log_json 1 --remote {bootstrap_ip} --port 30303 ' \
+use_impls = ['go']
+
+# this must be the same as in ../ansible/group_vars/all
+g_boot_public_key = '829bb728a1b38d2e3bb8288d750502f7dce2ee329aaebf48ddc54e0cfc8003b3068fe57e20277ba50e42826c4d2bfcb172699e108d9e90b3339f8b6589449faf'
+
+docker_run_args = {}
+docker_run_args['go'] = '-port=30000 -loglevel=5 -bootnodes=enode://{bootstrap_public_key}@{bootstrap_ip}:30303 -maxpeer={req_num_peers}'
+docker_run_args['python'] = '--logging :debug --log_json 1 --remote {bootstrap_ip} --port 30303 ' \
                   '--mining {mining_cpu_percentage} --peers {req_num_peers} --address {coinbase}'
 teees_args = '{elarch_ip} guid,{pubkey_hex}'
 
 mining_cpu_percentage = 50
 req_num_peers = 4
 
-ansible_args = ['-u', 'ubuntu', '--private-key=../ansible/system-testing.pem']
+ansible_args = ['-u', 'ubuntu', '--private-key=../ansible/system-testing.pem', '-vv']
 
 
 def mk_inventory_executable(inventory):
@@ -39,7 +44,8 @@ def mk_inventory_executable(inventory):
 def exec_playbook(inventory, playbook):
     fn = mk_inventory_executable(inventory)
     # replace for go with --tags=go or delete to address both
-    args = ['ansible-playbook', '../ansible/%s' % playbook, '-i', fn, '--tags=python'] + ansible_args
+    impls = ','.join(use_impls)
+    args = ['ansible-playbook', '../ansible/%s' % playbook, '-i', fn, '--tags=%s' % impls] + ansible_args
     print 'executing', ' '.join(args)
     result = subprocess.call(args)
     if result:
@@ -55,6 +61,8 @@ def start_clients(clients=[]):
     inventory = Inventory()
     clients = clients or list(inventory.clients)
     inventory.inventory['client_start_group'] = dict(children=clients, hosts=[])
+    # print clients
+    # quit()
     assert inventory.es
     assert inventory.boot
     for client in clients:
@@ -63,19 +71,23 @@ def start_clients(clients=[]):
         pubkey = nodeid_tool.topub(ext_id)
         coinbase = nodeid_tool.coinbase(ext_id)
         d = dict(hosts=inventory.inventory[client], vars=dict())
-        dra = docker_run_args.format(bootstrap_ip=inventory.boot,
+        dra_go = docker_run_args['go'].format(bootstrap_public_key=g_boot_public_key, bootstrap_ip=inventory.boot,
+                                     req_num_peers=req_num_peers)
+        dra_python = docker_run_args['python'].format(bootstrap_ip=inventory.boot,
                                      mining_cpu_percentage=mining_cpu_percentage,
                                      req_num_peers=req_num_peers,
                                      coinbase=coinbase)
-        # add here go parmeters 
         d['vars']['docker_run_args'] = {} 
-        d['vars']['docker_run_args']['python'] = dra
+        d['vars']['docker_run_args']['go'] = dra_go
+        d['vars']['docker_run_args']['python'] = dra_python
         d['vars']['docker_container_id'] = {}
-        d['vars']['docker_container_id']['python'] = client
+        d['vars']['docker_container_id']['go'] = 'docker_go'
+        d['vars']['docker_container_id']['python'] = 'docker_python'
         d['vars']['docker_tee_args'] = {}
+        d['vars']['docker_tee_args']['go'] = teees_args.format(elarch_ip=inventory.es, pubkey_hex=pubkey)
         d['vars']['docker_tee_args']['python'] = teees_args.format(elarch_ip=inventory.es, pubkey_hex=pubkey)
         inventory.inventory[client] = d
-#    print json.dumps(inventory.inventory, indent=2)
+    print json.dumps(inventory.inventory, indent=2)
     exec_playbook(inventory.inventory, playbook='client-start.yml')
 
 
@@ -89,10 +101,9 @@ def stop_clients(clients=[]):
     for client in clients:
         assert client
         d = dict(hosts=inventory.inventory[client], vars=dict())
-        # add the same for go
         d['vars']['docker_container_id'] = {}
-        d['vars']['docker_container_id']['python'] = client
-		# add the same container id for go
+        d['vars']['docker_container_id']['go'] = 'docker_go' 
+        d['vars']['docker_container_id']['python'] = 'docker_python'
         inventory.inventory[client] = d
 #    print json.dumps(inventory.inventory, indent=2)
     exec_playbook(inventory.inventory, playbook='client-stop.yml')
@@ -103,7 +114,8 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     sys.argv = sys.argv[:1]  # ec2.py peeks into args, so delete passing-variables-on-the-command-line
     if 'start' in args:
-        start_clients()
+          start_clients()
+    #     start_clients([u'tag_Name_client-01'])
     elif 'stop' in args:
         stop_clients()
     else:
