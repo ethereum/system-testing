b6247a926f961dda8aa80ac6d10baef5a612c0f8
sveneh <sven@ethdev.com>
2015-03-01 14:26:38 +0100

    enables externa rpc port for go clients - closes #53, fixes some start / stop issues

--- scenarios/clients.py
+++ scenarios/clients.py
@@ -22,7 +22,7 @@ use_impls = ['go']
 g_boot_public_key = '829bb728a1b38d2e3bb8288d750502f7dce2ee329aaebf48ddc54e0cfc8003b3068fe57e20277ba50e42826c4d2bfcb172699e108d9e90b3339f8b6589449faf'
 
 docker_run_args = {}
-docker_run_args['go'] = '-port=30000 -loglevel=1000 -logformat=raw ' \
+docker_run_args['go'] = '-port=30000 -rpcaddr=0.0.0.0 -loglevel=1000 -logformat=raw ' \
     '-bootnodes=enode://{bootstrap_public_key}@{bootstrap_ip}:30303 ' \
     '-maxpeer={req_num_peers} ' \
     '-nodekeyhex={privkey}'
@@ -32,9 +32,9 @@ docker_run_args['python'] = '--logging :debug --log_json 1 --remote {bootstrap_i
 teees_args = '{elarch_ip} guid,{pubkey_hex}'
 
 mining_cpu_percentage = 50
-req_num_peers = 4
 
-ansible_args = ['-u', 'ubuntu', '--private-key=../ansible/system-testing.pem'] #add -vv for debug output
+ansible_args = ['-u', 'ubuntu', '--private-key=../ansible/system-testing.pem', '-vv'] #add -vv for debug output
+
 
 
 def mk_inventory_executable(inventory):
@@ -60,7 +60,7 @@ def exec_playbook(inventory, playbook):
         print 'success'
 
 
-def start_clients(clients=[],maxnumpeer=4):
+def start_clients(clients=[],maxnumpeer=7):
     """
     start all clients with a custom config (nodeid)
     """
@@ -101,7 +101,7 @@ def start_clients(clients=[],maxnumpeer=4):
         d['vars']['docker_tee_args']['python'] = teees_args.format(
             elarch_ip=inventory.es, pubkey_hex=pubkey)
         inventory.inventory[client] = d
-    print json.dumps(inventory.inventory, indent=2)
+    # print json.dumps(inventory.inventory, indent=2)
     exec_playbook(inventory.inventory, playbook='client-start.yml')
 
 
@@ -129,9 +129,10 @@ if __name__ == '__main__':
     # ec2.py peeks into args, so delete passing-variables-on-the-command-line
     sys.argv = sys.argv[:1]
     if 'start' in args:
-        start_clients()
-    #     start_clients([u'tag_Name_client-01'])
+        # start_clients()
+        start_clients([u'tag_Name_ST-host-00000'])
     elif 'stop' in args:
-        stop_clients()
+        # stop_clients()
+        stop_clients([u'tag_Name_ST-host-00000'])
     else:
         print 'usage:%s start|stop' % sys.argv[0]
