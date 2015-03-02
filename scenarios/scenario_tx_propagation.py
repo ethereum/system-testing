f72c25d1027916a5879b51cded8e1b92a5d9f03d
Heiko Heiko <heiko@heiko.org>
2015-03-02 20:20:20 +0100

    json rpc and logging

--- scenarios/scenario_tx_propagation.py
+++ scenarios/scenario_tx_propagation.py
@@ -2,16 +2,16 @@ import subprocess
 from base import Inventory
 import nodeid_tool
 from clients import start_clients, stop_clients
-from eshelper import consensus, tx_propagation
-import random
+from eshelper import tx_propagation, log_scenario
+from rpc import coinbase, balance, transact
 import time
 import sys
 
-"""
-use json rpc
-curl -X POST --data '{"jsonrpc":"2.0","method":"eth_transact","params":[{"to":"0x12312312", value: "0x00...123"}],"id":1}' http://localhost:8080
-"""
 max_time_to_reach_consensus = 10
+
+
+def Ox(x):
+    return '0x' + x
 
 
 def scenario():
@@ -31,54 +31,50 @@ def scenario():
     clients = list(inventory.clients)
 
     # FIXME reset everything
+    log_scenario('tx_propagation', event='started')
 
     if True:
         # stop all clients
+        log_scenario('tx_propagation', event='stopping_clients')
         stop_clients(clients=clients[1:])
+        log_scenario('tx_propagation', event='stopping_clients.done')
+        log_scenario('tx_propagation', event='starting_one_client')
         start_clients(clients=clients[:1])
-
+        log_scenario('tx_propagation', event='starting_one_client.done')
         # mine a bit
         blocktime = 12
-        time.sleep(blocktime * len(clients) * 1.5)
+        delay = blocktime * 1.5
+        log_scenario('tx_propagation', event='waiting', delay=delay)
+        time.sleep(delay)
 
         # start other clients
+        log_scenario('tx_propagation', event='starting_other_clients')
         start_clients(clients=clients[1:])
+        log_scenario('tx_propagation', event='starting_other_clients.done')
 
     # create tx
     sender = clients[0]
-
-    ext_id = str(sender)
-#    pubkey = nodeid_tool.topub(ext_id)
-    privkey = nodeid_tool.topriv(ext_id)
-    sending_address = nodeid_tool.coinbase(ext_id)
-
     recipient = clients[1]
-    receiving_address = nodeid_tool.coinbase(str(recipient))
+    # sending_address = Ox(nodeid_tool.coinbase(str(sender)))  # FIXME
+    receiving_address = Ox(nodeid_tool.coinbase(str(recipient)))
 
     rpc_host = inventory.inventory[sender][0]
-    rpc_port = str(22000)
-    value = str(100)
+    rpc_port = 20000  # hard coded FIXME if we get multiple clients per ec
+    endpoint = 'http://%s:%d' % (rpc_host, rpc_port)
 
-    # dump account
-    # sending_address = '6c386a4b26f73c802f34673f7248bb118f97424a'
-    print 'sending address', sending_address
+    sending_address = coinbase(endpoint)
 
-    args = ['pyethclient', '-H', rpc_host, '-p', rpc_port, 'getbalance', sending_address]
-    print 'executing', ' '.join(args)
-    result = subprocess.call(args)
-    if result:
-        print 'failed'
+    value = 100
+    assert value < balance(endpoint, sending_address)
+    log_scenario('tx_propagation', event='sending_transaction',
+                 sender=sending_address, to=receiving_address, value=value)
+    tx = transact(endpoint, sender=sending_address, to=receiving_address, value=value)
+    log_scenario('tx_propagation', event='sending_transaction.done', result=tx)
 
-    # pyethclient quicktx <to> <value> <data_hex> <pkey_hex>
-
-    args = ['pyethclient', '-H', rpc_host, '-p', rpc_port]
-    args += ['quicktx', receiving_address, value, '', privkey]
-    print 'executing', ' '.join(args)
-    result = subprocess.call(args)
-    if result:
-        print 'failed'
-
+    log_scenario('tx_propagation', event='waiting', delay=max_time_to_reach_consensus)
     time.sleep(max_time_to_reach_consensus)
+    log_scenario('tx_propagation', event='waiting.done')
+
     num_agreeing_clients = tx_propagation(offset=max_time_to_reach_consensus * 2)
     print '%d out of %d clients received a tx' % (num_agreeing_clients, len(clients))
     return num_agreeing_clients == len(clients)
