f4b3b72d557d05bd42f4b38386d278c26daf94c4
sveneh <sven@ethdev.com>
2015-03-16 12:19:28 +0100

    --force-mining added for cpp client

--- scenarios/clients.py
+++ scenarios/clients.py
@@ -28,7 +28,7 @@ docker_run_args['go'] = '--port=30000 --rpcaddr=0.0.0.0 --rpcport=20000 --loglev
     '--mine={mining_state}'
 
 docker_run_args['cpp'] = '--verbosity 9 --structured-logging --json-rpc-port 21000 --listen 31000 --upnp off ' \
-    ' --public-ip {client_ip} --remote {bootstrap_ip} --peers {req_num_peers}, --mining {mining_state}'
+    ' --public-ip {client_ip} --remote {bootstrap_ip} --peers {req_num_peers} {mining_state}'
 
 docker_run_args['python'] = '--logging :debug --log_json 1 --remote {bootstrap_ip} --port 30303 ' \
     '--mining {mining_state} --peers {req_num_peers} --address {coinbase}'
@@ -102,7 +102,7 @@ def start_clients(clients=[], req_num_peers=7, impls=['go'], boot=0, enable_mini
         dra['cpp'] = docker_run_args['cpp'].format(bootstrap_ip=bt['ip'],
                                                       client_ip=inventory.instances[client],
                                                       req_num_peers=req_num_peers,
-                                                      mining_state='on' if enable_mining else 'off')
+                                                      mining_state='--force-mining --mining on' if enable_mining else '')
 
         dra['python'] = docker_run_args['python'].format(bootstrap_ip=bt['ip'],
                                                       req_num_peers=req_num_peers,
@@ -148,7 +148,7 @@ if __name__ == '__main__':
     if 'start' in args:
         log_scenario(name='cmd_line', event='start_clients')
         # start_clients()
-        start_clients([u'tag_Name_ST-host-00000'], impls=['go'], boot=0)
+        start_clients([u'tag_Name_ST-host-00000'], impls=['cpp'], boot=1, enable_mining=True)
         log_scenario(name='cmd_line', event='start_clients.done')
     elif 'stop' in args:
         log_scenario(name='cmd_line', event='stop_clients')
