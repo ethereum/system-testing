8ad1e2b477c0b29765e911732be74817d8dea673
sveneh <sven@ethdev.com>
2015-04-25 00:13:08 +0200

    fixed regressions due to changed Dockerfile in ethereum-go. Also fixes other regressions from AMI save feature

--- testbed-setup-from-save.sh
+++ testbed-setup-from-save.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 EC2_REGION=${1:-us-west-1} 
 SCRIPTDIR=`dirname $(readlink --canonicalize $0)`
@@ -18,8 +19,10 @@ echo "Setting up in $EC2_REGION region from previously saved state."
 echo "This will only work if instance configuration was previously saved for this region!"
 read -p "[Enter] to continue if region is right."
 
-ansible-playbook ec2-setup-from-save.yml --extra-vars=ec2_region=$EC2_REGION --inventory-file=inventory/hosts && \
-# still needs to create dag
-ansible-playbook client-prepare.yml  && \
-ansible-playbook client-start-bootstrap.yml && \
+ansible-playbook ec2-setup-from-save.yml --extra-vars=ec2_region=$EC2_REGION --inventory-file=inventory/hosts
+.inventory/ec2.py --refresh-cache 
+ansible-playbook client-prepare.yml
+ansible-playbook client-start-bootstrap.yml
 ansible-playbook elarch-start.yml
+
+# still needs to create dag, so it's not as fast as it could be
--- testbed-setup.sh
+++ testbed-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 EC2_REGION=${1:-us-west-1} 
 SCRIPTDIR=`dirname $(readlink --canonicalize $0)`
@@ -16,9 +17,10 @@ sed --in-place=.bak "s/^regions =.*/regions = $EC2_REGION/" inventory/ec2.ini
 echo "Setting up in $EC2_REGION region."
 read -p "[Enter] to continue if region is right."
 
-# --tag selecets implementation of bootstrap node
-ansible-playbook ec2-setup.yml --extra-vars=ec2_region=$EC2_REGION --inventory-file=inventory/hosts && \
-ansible-playbook client-setup.yml && \
-ansible-playbook client-setup-bootstrap.yml && \  
-ansible-playbook client-start-bootstrap.yml && \
+
+ansible-playbook ec2-setup.yml --extra-vars=ec2_region=$EC2_REGION --inventory-file=inventory/hosts
+./inventory/ec2.py --refresh-cache
+ansible-playbook client-setup.yml
+ansible-playbook client-setup-bootstrap.yml
+ansible-playbook client-start-bootstrap.yml
 ansible-playbook elarch-setup.yml 	
